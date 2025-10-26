c57a3e1d1592d96b19ef685257644e5c90b34013
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-10-26 16:46:17 +0100

    Created a connection with the mysql database. 1. Installed the dovenv express mysql jsonwebtoken bcrypt packages. 2. created and implemented the server.js file for the entry point of the program 3. created app.js file for the connection logic to the db 4. created .env file to hide connection credentials

---
+++ config/app.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+require('dotenv').config();
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+connection.connect(function(err){
+    if(err)
+        console.error('error occured while connection', err);
+    else
+        console.log("Successfully connected to database");
+});
+module.exports = connection;
