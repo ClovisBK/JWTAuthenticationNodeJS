4bc45e9111fa25f543f01d85cdd167a1493ae107
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2025-11-17 08:25:09 +0100

    changed back to Gmail SMTP with google password

--- Utils/SendEmail.js
+++ Utils/SendEmail.js
@@ -1,20 +1,25 @@
-const {Resend} = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require("nodemailer");
 
 const sendEmail = async ({to, subject, html}) => {
 
     try{
 
-        const response = await resend.emails.send({
-
-            from: "kebehCard <onboarding@resend.dev>",
+        const transporter =  nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth:{
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+        const info = await transporter.sendMail({
+            from:  `"Kebeh Card" <${process.env.EMAIL_FROM}`,
             to,
             subject,
-            html
-        }      
-    );
-        console.log("Resend email sent:", response);
+            html,
+        });
+        console.log("Resend email sent:", info.messageId);
     }catch(error){
         console.error("Resend email error:", error);
         throw new Error("Email sending failed");
